5250b52816c62712f68d36da20b55bd8a756406a
Taylor Lafrinere <lafritay@gmail.com>
2017-06-26 18:43:07 -0400

    Adding new properties to RecentBill

--- ProPublicaCongressAPI/InternalModels/RecentBill.cs
+++ ProPublicaCongressAPI/InternalModels/RecentBill.cs
@@ -4,6 +4,12 @@ namespace ProPublicaCongressAPI.InternalModels
 {
     internal class RecentBill
     {
+        [JsonProperty("bill_id")]
+        public string BillId { get; set; }
+
+        [JsonProperty("bill_type")]
+        public string BillType { get; set; }
+
         [JsonProperty("number")]
         public string BillNumber { get; set; }
 
@@ -13,17 +19,50 @@ namespace ProPublicaCongressAPI.InternalModels
         [JsonProperty("title")]
         public string BillTitle { get; set; }
 
+        [JsonProperty("sponsor_id")]
+        public string SponsorId { get; set; }
+
         [JsonProperty("sponsor_uri")]
         public string SponsorMemberDetailUrl { get; set; }
 
+        [JsonProperty("gpo_pdf_uri")]
+        public string BillDocumentPdfUrl { get; set; }
+
+        [JsonProperty("congressdotgov_url")]
+        public string CongressDotGovUrl { get; set; }
+
+        [JsonProperty("govtrack_url")]
+        public string GovTrackUrl { get; set; }
+
         [JsonProperty("introduced_date")]
         public string DateIntroduced { get; set; }
+
+        [JsonProperty("active")]
+        public bool Active { get; set; }
+
+        [JsonProperty("house_passage")]
+        public string HousePassage { get; set; }
+
+        [JsonProperty("senate_passage")]
+        public string SenatePassage { get; set; }
+
+        [JsonProperty("enacted")]
+        public string Enacted { get; set; }
+
+        [JsonProperty("vetoed")]
+        public string Vetoed { get; set; }
 
         [JsonProperty("cosponsors")]
         public int CosponsorCount { get; set; }
 
         [JsonProperty("committees")]
         public string Committees { get; set; }
+
+        [JsonProperty("committee_codes")]
+        public string[] CommitteeCodes { get; set; }
+
+        [JsonProperty("subcommittee_codes")]
+        public string[] SubcommitteeCodes { get; set; }
 
         [JsonProperty("primary_subject")]
         public string PrimarySubject { get; set; }
@@ -33,5 +72,11 @@ namespace ProPublicaCongressAPI.InternalModels
 
         [JsonProperty("latest_major_action")]
         public string LatestMajorAction { get; set; }
+
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+
+        [JsonProperty("summary_short")]
+        public string SummaryShort { get; set; }
     }
 }
